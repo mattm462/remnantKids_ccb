cd782424ee127cba73115e09f6f79e3ffa53ecc5
Matthew McCurdy <matt@mccurdyfamily.net>
2016-03-06 15:19:19 -0500

    checkin ui

---
+++ gm_scripts/remnantKidsCheckIn.user.js
@@ -0,0 +1,34 @@
+// ==UserScript==
+// @name        remnantKidsCheckIn
+// @namespace   remnantKidsCheckIn
+// @description remnantKidsCheckIn
+// @include     https://remnantchurch.ccbchurch.com/checkin_main.php*
+// @version     1
+// @resource cssSrc https://raw.githubusercontent.com/mattm462/remnantKids_ccb/master/styles/custom_kiosk.css
+// @require https://raw.githubusercontent.com/mattm462/remnantKids_ccb/master/scripts/custom_kiosk_manned.js
+// @grant    GM_addStyle
+// @grant    GM_getResourceText
+// ==/UserScript==
+
+
+GM_addStyle(GM_getResourceText("cssSrc"));
+console.log("[SUCCESS] Custom RemnantKids styles applied");
+
+
+
+// function addStyle(style) {
+// var head = document.getElementsByTagName("HEAD")[0];
+// // var ele = head.appendChild(window.document.createElement( 'style' ));
+// //   ele.type="text/css";
+// // ele.innerHTML = style;
+// // return ele;
+//   var link = head.appendChild(window.document.createElement( 'link' ));
+// link.type = 'text/css';
+// link.rel = 'stylesheet';
+// link.href = 'http://www.matthewmccurdy.com/remnantDesign.css'
+// return ele;
+// }
+
+    
+
+// addStyle(); 
