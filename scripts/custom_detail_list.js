a4826716b19d63065e5697de40d5f63fbe4f8810
Matthew McCurdy <matt@mccurdyfamily.net>
2016-03-20 13:51:12 -0400

    detail list prevent changes

--- scripts/custom_detail_list.js
+++ scripts/custom_detail_list.js
@@ -8,7 +8,16 @@ function hideAdmin() {
 function showAdmin() {
 	$("#header .right .group").show();
 	$(".label-quantity").show();
+	$('.disabledRow').removeClass('disabledRow');
+	$('#note').remove();
 };
+
+
+var $childRow = $('div.group-events').find('div.group-event');
+if( $childRow.hasClass('event-checked-in') ){ // .hasClass() returns BOOLEAN true/false
+  $childRow.parents('div.item').addClass('disabledRow');
+  $('body').prepend('<div id="note"><p>A child has already been checked-in. See RemnantKids volunteer to make changes.<br/>To check-out, please claim your child and scan his or her nametag.</p></div>' );
+}
 
 
 hideAdmin(); //enter kiosk mode on launch
