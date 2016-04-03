fba1e3b2761819aec33bd8c2e99ea5bc810fbd95
Matthew McCurdy <matt@mccurdyfamily.net>
2016-04-03 13:09:42 -0400

    add disabled row to hide and show functions

--- scripts/custom_detail_list.js
+++ scripts/custom_detail_list.js
@@ -3,6 +3,7 @@
 function hideAdmin() {
 	$("#header .right .group").hide();
 	$(".label-quantity").hide();
+	disableRow(); //block out rows already checked-in
 };
 
 function showAdmin() {
@@ -12,15 +13,18 @@ function showAdmin() {
 	$('#customNote').remove();
 };
 
-hideAdmin(); //enter kiosk mode on launch
 
-$(function() {
+
+function disableRow() {
 	var $childRow = $('div.group-events').find('div.group-event');
 	if( $childRow.hasClass('event-checked-in') ){ // .hasClass() returns BOOLEAN true/false
 	  $childRow.parents('div.item').addClass('disabledRow');
 	  $('.event-checked-in').append("<div class=\"title\" id=\"customNote\"><span>See a volunteer to make changes.</span><br><span>To check-out, scan the child's nametag at the main screen.</span></div>" );
 	}
+};
 
+$(function() {
+	hideAdmin(); //enter kiosk mode on launch
 	//mousetrap key shortcuts
 	Mousetrap.bind(['esc'], hideAdmin); //hide admin stuff with esc
 	Mousetrap.bind(['option+a', 'alt+a'], showAdmin); //Enter Admin mode with alt-a or option-a
